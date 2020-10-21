1ad5037cd4806b8ed6b6962f29c426dd59d768d5
Emiel Legrand <emiel.legrand@gmail.com>
2020-10-21 13:01:24 +0200

    overall transition and output functions

---
+++ part 2/f.m
@@ -0,0 +1,21 @@
+function [x_new] = f(x, k, u, par)
+% Overall state transition function, including both links ud and o1d.
+    N = numel(x)/2;
+    x_ud = x(1:N); % States for link ud
+    x_o1d = x((N+1):end); % States for link 01d
+    
+    % Update states for link ud
+    x_ud_new = state_transition(x_ud, k, u, ...
+                                @(k) alpha_ud_enter(k, par.ud), ...
+                                @(k) Cp_ud_output(k, par.ud), ...
+                                par.ud);
+    
+    % Update states for link o1d
+    x_o1d_new = state_transition(x_o1d, k, par.c - u, ...
+                                 @(k) alpha_o1d_enter(k, par.o1d), ...
+                                 @(k) Cp_o1d_output(k, par.o1d), ...
+                                 par.o1d);
+                             
+    x_new = [x_ud_new; x_o1d_new];
+end
+
---
+++ part 2/g.m
@@ -0,0 +1,5 @@
+function [y] = g(x, u, par)
+% Output
+    y = par.ud.c*[0 0 0 1 0 0 0 1]*x; % Both time steps are equal
+end
+
