fe41e7fff8fe4b35f3deb65ae7ada1c9a926f970
fphassan <feres1995@gmail.com>
2020-10-25 18:53:45 +0100

    Integer ga optimization added
    Added integer genetic algorithm optimization

--- part 2/mapvariables.m
+++ part 2/mapvariables.m
@@ -2,9 +2,8 @@ function u = mapvariables (u_disc)
 
 % Discrete time set for green light time
 discrete_set= (15:5:45);
-u = zeros(60,1);
-% Map u from integer values used by GA to the discrete values required
 
+% Map u from integer values used by GA to the discrete values required
 u = discrete_set(u_disc);
 end
 
