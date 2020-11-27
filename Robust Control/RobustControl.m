2c06002acbbf0688ad8850fa438c170c8286b688
Emiel Legrand <emiel.legrand@gmail.com>
2020-11-27 20:04:04 +0100

    final

--- Robust Control/RobustControl.m
+++ Robust Control/RobustControl.m
@@ -12,29 +12,26 @@ margin(G);
 pzmap(G);
 close all
 
-%% Root locus
-figure
-rlocus(-G)
-
-%% proportional gain action (first plot)
-G_margins = allmargin(-G); % Compute all the margins
-for K = linspace(0, G_margins.GainMargin, 5)
-    step(feedback(-K*G, 1), 0:0.2:100);
-    hold on; grid; grid minor;
-end
-
 %% PI Implementation (second plot)
 Kp = -16.21/3.2; % Tuning parameters based on Ziegler and Nichols 
 Ti = 31/1.5;
 % Td = -10; % No derivative action is needed
 % Tf = 50;
 K = pid(Kp, Kp/Ti) %, Kp*Td, Tf);
-figure('Name', 'Pi FRF') 
-L = minreal(K*G)
+figure('Name', 'Pi FRF')
+L = minreal(K*G);
 T = minreal(L/(1 + L), 1e-7);
-step(feedback(L,1)) 
-stepinfo(T,'SettlingTimeThreshold', 0.01)
 
+[y_step, t_step] = step(feedback(L,1)); 
+plot(t_step, y_step);
+title('\textbf{Step response for PI control}', 'interpreter', 'latex', 'fontsize', 13)
+xlabel('Time (s)', 'interpreter', 'latex', 'fontsize', 12)
+ylabel('Amplitude', 'interpreter', 'latex', 'fontsize', 12)
+format_axes(gca);
+% stepinfo(T,'SettlingTimeThreshold', 0.01)
+
+
+%%
 [y_step, t_step] = step(T);
 plot_step = plot(t_step, y_step, 'Color', '#27ae60');
 set(plot_step, 'LineWidth', 1.7);
@@ -46,17 +43,14 @@ title('\textbf{Step response}', 'interpreter', 'latex', 'fontsize', 13);
 format_axes(ax);
 
 %% Low pass idea
-close all
 w_cut = 0.1;
 lowpass = tf([0 0 w_cut^2], [1 2*w_cut*0.83 w_cut^2]);
-bode(lowpass); hold on;
 L = minreal(lowpass*K*G);
 T = minreal(L/(1 + L), 1e-7);
 
 stepinfo(T,'SettlingTimeThreshold', 0.01)
 
 figure
-margin(T)
 [y_step, t_step] = step(T);
 plot_step = plot(t_step, y_step, 'Color', '#27ae60');
 set(plot_step, 'LineWidth', 1.7);
@@ -98,28 +92,50 @@ stepinfo(T, 'SettlingTimeThreshold', 0.01)
 %% Plots
 close all;
 
+%% proportional gain action (first plot)
+% G_margins = allmargin(-G); % Compute all the margins
+% for K = linspace(1, G_margins.GainMargin, 4)
+%     [y_step, t_step] = step(feedback(-K*G, 1), 0:0.2:80);
+%     plot(t_step, y_step, 'DisplayName', sprintf('$K = -%.1f$', K));
+%     hold on; grid; grid minor;
+% end
+% format_axes(gca)
+% title('\textbf{Step response for proportional gain}', 'interpreter', 'latex', 'fontsize', 13)
+% xlabel('Time (s)', 'interpreter', 'latex', 'fontsize', 12)
+% ylabel('Amplitude', 'interpreter', 'latex', 'fontsize', 12)
+% legend('Interpreter', 'latex')
+
+%% Root locus
+% figure
+% h = rlocusplot(-G);
+% h.AxesGrid.XUnits = '';
+% h.AxesGrid.YUnits = '';
+% xlabel('');
+% ylabel('');
+% title('\textbf{Root locus}', 'interpreter', 'latex', 'fontsize', 13);
+
 % Step response
-[y_step, t_step] = step(T);
-plot_step = plot(t_step, y_step, 'Color', '#27ae60');
-set(plot_step, 'LineWidth', 1.7);
-ax = gca;
-xlim([0 max(t_step)])
-xlabel('Time [s]', 'interpreter', 'latex', 'fontsize', 12);
-ylabel('Amplitude', 'interpreter', 'latex', 'fontsize', 12);
-title('\textbf{Step response}', 'interpreter', 'latex', 'fontsize', 13);
-format_axes(ax);
+% [y_step, t_step] = step(T);
+% plot_step = plot(t_step, y_step, 'Color', '#27ae60');
+% set(plot_step, 'LineWidth', 1.7);
+% ax = gca;
+% xlim([0 max(t_step)])
+% xlabel('Time [s]', 'interpreter', 'latex', 'fontsize', 12);
+% ylabel('Amplitude', 'interpreter', 'latex', 'fontsize', 12);
+% title('\textbf{Step response}', 'interpreter', 'latex', 'fontsize', 13);
+% format_axes(ax);
 
 % Controller effort
-figure
-[y_input] = step(K/(1+K*G), t_step);
-plot_step = plot(t_step, y_input, 'Color', '#27ae60');
-set(plot_step, 'LineWidth', 1.7);
-ax = gca;
-xlim([0 max(t_step)])
-xlabel('Time [s]', 'interpreter', 'latex', 'fontsize', 12);
-ylabel('Amplitude', 'interpreter', 'latex', 'fontsize', 12);
-title('\textbf{Controller effort}', 'interpreter', 'latex', 'fontsize', 13);
-format_axes(ax);
+% figure
+% [y_input] = step(K/(1+K*G), t_step);
+% plot_step = plot(t_step, y_input, 'Color', '#27ae60');
+% set(plot_step, 'LineWidth', 1.7);
+% ax = gca;
+% xlim([0 max(t_step)])
+% xlabel('Time [s]', 'interpreter', 'latex', 'fontsize', 12);
+% ylabel('Amplitude', 'interpreter', 'latex', 'fontsize', 12);
+% title('\textbf{Controller effort}', 'interpreter', 'latex', 'fontsize', 13);
+% format_axes(ax);
 
 %% Sensitivity and complementary sensitivity
 % sens_plot = bodeplot(S);
@@ -134,24 +150,24 @@ format_axes(ax);
 % format_axes(ax);
 
 %% Complementary sensitivity
-comp_plot = bodeplot(T);
-opt = getoptions(comp_plot);
-opt.PhaseVisible = 'off';
-setoptions(comp_plot, opt);
-
-ax = gca;
-xlabel('$\omega$', 'interpreter', 'latex', 'fontsize', 12);
-ylabel('$|S|$', 'interpreter', 'latex', 'fontsize', 12);
-title('\textbf{Complementary sensitivity}', 'interpreter', 'latex', 'fontsize', 13);
-format_axes(ax);
+% comp_plot = bodeplot(T);
+% opt = getoptions(comp_plot);
+% opt.PhaseVisible = 'off';
+% setoptions(comp_plot, opt);
+% 
+% ax = gca;
+% xlabel('$\omega$', 'interpreter', 'latex', 'fontsize', 12);
+% ylabel('$|S|$', 'interpreter', 'latex', 'fontsize', 12);
+% title('\textbf{Complementary sensitivity}', 'interpreter', 'latex', 'fontsize', 13);
+% format_axes(ax);
 
 %% Pole-zero plot
-plot(pole(G), 'x');
-hold on;
-plot(zero(G), 'o');
-
-%%
-format_axes(gca);
+% plot(pole(G), 'x');
+% hold on;
+% plot(zero(G), 'o');
+% 
+% %%
+% format_axes(gca);
 
 %% Plant bode plot
 h = bodeplot(G);
@@ -163,7 +179,15 @@ setoptions(h, opt)
 title('\textbf{Plant Bode diagram}', 'Fontsize', 13);
 
 %% Plant Nyquist
-nyquistplot(-G)
+% nyquistplot(-G)
+
+%% Disturbance rejection
+[y_step, t_step] = step(S*G_MIMO(1, 3));
+plot(t_step, y_step, 'LineWidth', 1.7, 'Color', '#27ae60');
+format_axes(gca)
+title('\textbf{Disturbance rejection}', 'interpreter', 'latex', 'fontsize', 13)
+xlabel('Time (s)', 'interpreter', 'latex', 'fontsize', 12)
+ylabel('Amplitude', 'interpreter', 'latex', 'fontsize', 12)
 
 
 function format_axes(ax)
