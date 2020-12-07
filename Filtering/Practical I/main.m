6b15a5f366def7ab07f668b4ca0633b0c3491a37
Emiel Legrand <emiel.legrand@gmail.com>
2020-12-07 15:33:36 +0100

    het werkt en niemand weet waarom

--- Filtering/Practical I/main.m
+++ Filtering/Practical I/main.m
@@ -14,39 +14,12 @@ clear; clc; close all
 %% Assignment 1
 load('calibration.mat')
 
-c = 343; % speed of sound [m/s]
-k = 59; % number of pulses
-m = 7; % number of microphones
-
-% y = [y zeros(size(y,1),1)]; % Append column of zero's for speed
-
-% % Calculate measurement errors and estimate biases
-% for i = 1:k
-%     y(i,8) = mean(y(i,1:7)); % Average TOA for each time step
-%     for j = 1:m
-%         error(i,j) = y(i,j) - y(i,8); % Measurement errors at each timestep
-%         bias(j) = mean(error(:,j)); % Mean error of all timesteps to estimate bias
-%     end
-% end
-% 
-% % Estimate variance
-% for j = 1:m
-%     for i = 1:k
-%         efb(i,j) = error(i,j)-bias(j); % Measurement noise
-%     end
-%     variance(j) = var(efb(:,j)); % Variance of measurement noise
-% end
-% 
-% % Visualize measurement errors of microphone 1
-% H = histogram(efb(:,5),6);
-
-% -------------------------------------------------------------------------
 % Alternatively, without loops
 
 % Question 1a: Error per microphone
 y_calib = y/100; % This will be overwritten later by new data
 TOA_true = mean(y_calib, 2);
-mic_error = y_calib - repmat(TOA_true, 1, m);
+mic_error = y_calib - TOA_true;
 
 % Question 1b: Microphone bias
 mic_bias = mean(mic_error);
@@ -71,8 +44,8 @@ y_exp = y/100;
 N_exp = size(y_exp, 1);
 y_bias_correct = y_exp - repmat(mic_bias, N_exp, 1);
 
-theta_hat = zeros(3,N_exp+1);
-diagP = zeros(3, N_exp);
+theta_hat = nan(3,N_exp+1);
+diagP = nan(3, N_exp);
 theta_hat(:,1) = [0.1 0.6 0]';
 
 % Question 2b
@@ -103,25 +76,26 @@ function [theta, diagP] = nls(yk, noisevar, th_hat0, maxiter, mic_locations)
     count = 1;
     P = eye(numel(th_hat0));
     
-    while ~converged && ~max_iter_reached       
-       % Solve a linear least-squares model
-       F = Jacobian(theta, mic_locations);
-       yhat = f(theta, mic_locations);
-       d_theta = F\(yk - yhat);
-       P = P - P*F'*pinv(F*P*F' + inv(noisevar))*F*P;
-       
-       % Check convergence
-       if norm(d_theta) < 1e-6
-           converged = true;
-       elseif count > maxiter
-           max_iter_reached = true;
-           warning('Max iterations reached')
-       end
-       
-       count = count + 1;
-       theta = theta + d_theta;
-        
-    end 
+%     while ~converged && ~max_iter_reached       
+%        % Solve a linear least-squares model
+%        F = Jacobian(theta, mic_locations);
+%        yhat = f(theta, mic_locations);
+%        d_theta = F\(yk - yhat);
+%        P = P - P*F'*pinv(F*P*F' + inv(noisevar))*F*P;
+%        
+%        % Check convergence
+%        if norm(d_theta) < 1e-6
+%            converged = true;
+%        elseif count > maxiter
+%            max_iter_reached = true;
+%            warning('Max iterations reached')
+%        end
+%        
+%        count = count + 1;
+%        theta = theta + d_theta;
+%     end
+    
+    theta = lsqnonlin(@(x) yk - f(x, mic_locations), th_hat0);
     
     diagP = diag(P);
 end
@@ -140,8 +114,8 @@ function ftheta = f(theta, mic_locations)
     c = 343; % speed of sound in [m/s]
     
     % Normalize for cm (????)
-    ftheta = theta(3) ...
-             + vecnorm([theta(1) theta(2)] - mic_locations, 2, 2)/c;
+    ftheta = (theta(3) ...
+             + vecnorm([theta(1) theta(2)] - mic_locations, 2, 2)/c)/100;
     % Slightly verbose notation to avoid issues with theta being a column
     % vector
 end
