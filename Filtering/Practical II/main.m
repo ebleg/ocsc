571a7162b705137e5f3773a6d67dd5995d3b9d27
Emiel Legrand <emiel.legrand@gmail.com>
2021-01-04 21:21:25 +0100

    new student number

--- Filtering/Practical II/main.m
+++ Filtering/Practical II/main.m
@@ -5,7 +5,7 @@ clear; clc; close all;
 
 student_number = min(4446100, 4362152);
 
-exciteSystem2 = @(u, fs) exciteSystem(4434514, u, fs);
+exciteSystem2 = @(u, fs) exciteSystem(2512634, u, fs);
 
 %% Part 0: Experiment
 h = 1/30;
@@ -14,12 +14,12 @@ tin = 0:h:t_max;
 % u = zeros(size(tin))';
 % u(tin > 2) = 1000;
 
-u = square(tin).'*1e3;
+u = square(tin/2).'*1e3;
 % y = exciteSystem2(u, h);
-% save('system_output_h', 'y');
+% save('system_output_inv', 'y');
 
 %% Part 1: Data Preprocessing
-load 'system_output';
+load 'system_output_inv';
 
 % Spike removal
 y_filt = hampel(y, 4);
@@ -39,7 +39,7 @@ figure(2)
 yyaxis right; set(gca, 'YColor', 'black')
 stairs(tin, u);
 ylabel('Input'); axis('padded');
-xlim([2 4]);
+% xlim([2 4]);
 title('Input delay');
 xlabel('Time(s)'); grid;
 yyaxis left; set(gca, 'YColor', 'black')
@@ -70,8 +70,8 @@ line([n n], get(gca, 'YLim'), 'Color', 'black');
 %% System identification
 K = zeros(n, 1);
 [A, B, C, D, x0, sv] = subspaceID(u_filt, y_filt, 50, n, 'po-moesp');
-[pem.Abar, pem.Bbar, pem.C, pem.D, pem.K, pem.x0] = ...
-                   pem(A - K*C, B - K*D, C, D, K, x0, u_filt, y_filt, 300);
+% [pem.Abar, pem.Bbar, pem.C, pem.D, pem.K, pem.x0] = ...
+%                    pem(A - K*C, B - K*D, C, D, K, x0, u_filt, y_filt, 300);
 
 figure(5)
 semilogy(sv);
@@ -81,11 +81,11 @@ figure(6)
 y_ss = simsystem(A, B, C, D, x0, u_filt); hold on;
 stairs(tin, y_filt);
 
-y_pem = simsystem(pem.Abar, [pem.Bbar pem.K], pem.C, ...
-                            [pem.D zeros(1, 1)], pem.x0, [u_filt y_filt]);
+% y_pem = simsystem(pem.Abar, [pem.Bbar pem.K], pem.C, ...
+%                             [pem.D zeros(1, 1)], pem.x0, [u_filt y_filt]);
 
 stairs(tin, y_ss);
-stairs(tin, y_pem);
+% stairs(tin, y_pem);
 
 disp(vaf(y_filt, y_ss));
 
